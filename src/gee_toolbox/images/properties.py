f81a4b5d3ee4642cdcc0bbe563d327f080a5c65f
Erick G <9361923+silversky54@users.noreply.github.com>
2026-08-25 18:44:41 -0400

    feat: Add new module "images" with functions to add properties to Images

---
+++ src/gee_toolbox/images/properties.py
@@ -0,0 +1,24 @@
+import ee
+
+from gee_toolbox.dates.dates import ee_date_to_datetime
+
+
+def get_date_str(ee_image: ee.image.Image) -> str:
+    """Returns the date of an Image in string format 'YYYY-MM-DD' in UTC timezone
+
+    Args:
+        ee_image (ee.Image): Image with 'system:time_start' property
+
+    Returns:
+        str: Date in format "YYYY-MM-DD"
+
+    Raises:
+        ValueError: If the image doesn't have the property 'system:time_start'
+    """
+
+    img_date_in_ms = ee_image.get("system:time_start").getInfo()
+
+    if img_date_in_ms is None:
+        raise ValueError("Image does not have a 'system:time_start' property")
+
+    return ee_date_to_datetime(img_date_in_ms).strftime("%Y-%m-%d")
